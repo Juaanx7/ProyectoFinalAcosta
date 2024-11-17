b5282b45686cf710debbef25f5d96aad69351ae3
Juaanx7 <juaanx7@gmail.com>
2024-11-17 15:30:39 -0300

    mejora de estilo para confirmacion de compra

--- src/components/OrderConfirmation.jsx
+++ src/components/OrderConfirmation.jsx
@@ -1,13 +1,29 @@
 import { useParams } from "react-router-dom";
+import { Button, Card } from 'react-bootstrap'; // Importamos los componentes de Bootstrap
+import { Link } from 'react-router-dom';
+import '../styles/main.scss';
 
 function OrderConfirmation() {
   const { id } = useParams();
 
   return (
-    <div>
-      <h2>Confirmación de Compra</h2>
-      <p>¡Gracias por tu compra!</p>
-      <p>Tu orden ha sido registrada con el ID: {id}</p>
+    <div className="d-flex justify-content-center align-items-center" style={{ minHeight: '100vh' }}>
+      <Card className="card-custom">
+        <Card.Body>
+          <Card.Title className="card-title-custom">¡Gracias por tu compra!</Card.Title>
+          <Card.Text className="card-text-custom">
+            Tu orden ha sido registrada con el ID: <span className="order-confirmation-id">{id}</span>
+          </Card.Text>
+          <div className="button-container">
+            <Button variant="primary" as={Link} to="/cart" className="btn-checkout m-2">
+              Volver al Carrito
+            </Button>
+            <Button variant="secondary" as={Link} to="/" className="btn-checkout m-2">
+              Volver al Inicio
+            </Button>
+          </div>
+        </Card.Body>
+      </Card>
     </div>
   );
 }
