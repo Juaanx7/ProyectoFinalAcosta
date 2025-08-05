4828939152d5bbcfc91b927bad0b64f0be7f8c49
Juaanx7 <juaanx7@gmail.com>
2025-08-05 20:13:05 -0300

    solucion de sombra en botones

--- src/components/CartSummary.jsx
+++ src/components/CartSummary.jsx
@@ -1,21 +1,21 @@
-// src/components/CartSummary.jsx
 import { useCart } from "../context/CartContext";
 import { Link } from "react-router-dom";
-import { Card, Button } from "react-bootstrap";
+import { Card} from "react-bootstrap";
+import '../styles/CartSummary.scss';
 
 function CartSummary() {
   const { calculateTotalItems, calculateTotal } = useCart();
 
   return (
-    <Card className="cart-summary p-3 shadow-sm">
+    <Card className="cart-summary p-3">
       <h5>Resumen del carrito</h5>
       <p>Productos: {calculateTotalItems()}</p>
       <p>Total: ${calculateTotal()}</p>
-      <Link to="/cart">
-        <Button variant="primary" className="w-100 mt-2">Ver Carrito</Button>
+      <Link to="/cart" className="cart-btn-link">
+        <button className="cart-summary-btn">Ver Carrito</button>
       </Link>
-      <Link to="/checkout">
-        <Button variant="success" className="w-100 mt-2">Ir al Checkout</Button>
+      <Link to="/checkout" className="cart-btn-link">
+        <button className="cart-summary-btn">Ir al Checkout</button>
       </Link>
     </Card>
   );
